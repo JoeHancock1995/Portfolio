34f68e5055f15e3e67c43e237d07abbf91ce1a7b
Joe Hancock <joe.hancock95@gmail.com>
2019-06-23 12:46:54 -0500

    color update

---
+++ assets/js/screen.js
@@ -0,0 +1,17 @@
+<script>
+     
+     $(document).ready(function(){
+          
+          $(window).scroll(function(){
+
+            if($(window).scrollTop()>300){
+              $('nav').addClass('red');
+            }else{
+              $('nav').removeClass('red');
+            }
+
+          });
+
+     });
+
+  </script>

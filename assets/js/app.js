0f446037dfd5f77d131c96ba7246a4f74fdc73da
Joseph <joe.hancock95@gmail.com>
2020-08-25 14:41:20 -0500

    moved js into seperate file again

--- assets/js/app.js
+++ assets/js/app.js
@@ -77,3 +77,249 @@ showTime();
     animate();
     
 });
+
+import * as THREE from '../build/three.module.js';
+
+import { EffectComposer } from './jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from './jsm/postprocessing/RenderPass.js';
+import { ShaderPass } from './jsm/postprocessing/ShaderPass.js';
+import { BloomPass } from './jsm/postprocessing/BloomPass.js';
+import { CopyShader } from './jsm/shaders/CopyShader.js';
+
+var container;
+
+var camera, scene, renderer;
+
+var video, texture, material, mesh;
+
+var composer;
+
+var mouseX = 0;
+var mouseY = 0;
+
+var windowHalfX = window.innerWidth / 1;
+var windowHalfY = window.innerHeight / 1;
+
+var cube_count,
+
+  meshes = [],
+  materials = [],
+
+  xgrid = 40,
+  ygrid = 20;
+
+var startButton = document.getElementById('startButton');
+startButton.addEventListener('click', function () {
+
+  init();
+  animate();
+
+}, false);
+
+function init() {
+
+  var overlay = document.getElementById('overlay');
+  overlay.remove();
+
+  container = document.createElement('div');
+  document.body.appendChild(container);
+
+  camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 10000);
+  camera.position.z = 300;
+
+  scene = new THREE.Scene();
+
+  var light = new THREE.DirectionalLight(0xffffff);
+  light.position.set(0.5, 1, 1).normalize();
+  scene.add(light);
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  container.appendChild(renderer.domElement);
+
+  video = document.getElementById('video');
+  video.play();
+  video.addEventListener('play', function () {
+    this.currentTime = 3;
+  }, false);
+
+  texture = new THREE.VideoTexture(video);
+
+  //
+
+  var i, j, ux, uy, ox, oy,
+    geometry,
+    xsize, ysize;
+
+  ux = 1 / xgrid;
+  uy = 1 / ygrid;
+
+  xsize = 200 / xgrid;
+  ysize = 125 / ygrid;
+
+  var parameters = { color: 0xffffff, map: texture };
+
+  cube_count = 0;
+
+  for (i = 0; i < xgrid; i++)
+    for (j = 0; j < ygrid; j++) {
+
+      ox = i;
+      oy = j;
+
+      geometry = new THREE.BoxBufferGeometry(xsize, ysize, xsize);
+
+      change_uvs(geometry, ux, uy, ox, oy);
+
+      materials[cube_count] = new THREE.MeshLambertMaterial(parameters);
+
+      material = materials[cube_count];
+
+      material.hue = i / xgrid;
+      material.saturation = 1 - j / ygrid;
+
+      material.color.setHSL(material.hue, material.saturation, 0.5);
+
+      mesh = new THREE.Mesh(geometry, material);
+
+      mesh.position.x = (i - xgrid / 2) * xsize;
+      mesh.position.y = (j - ygrid / 2) * ysize;
+      mesh.position.z = 0;
+
+      mesh.scale.x = mesh.scale.y = mesh.scale.z = 1;
+
+      scene.add(mesh);
+
+      mesh.dx = 0.001 * (0.05 - Math.random());
+      mesh.dy = 0.001 * (0.05 - Math.random());
+
+      meshes[cube_count] = mesh;
+
+      cube_count += 1;
+
+    }
+
+  renderer.autoClear = false;
+
+  document.addEventListener('mousemove', onDocumentMouseMove, false);
+
+  // postprocessing
+
+  var renderModel = new RenderPass(scene, camera);
+  var effectBloom = new BloomPass(1);
+  var effectCopy = new ShaderPass(CopyShader);
+
+  composer = new EffectComposer(renderer);
+
+  composer.addPass(renderModel);
+  composer.addPass(effectBloom);
+  composer.addPass(effectCopy);
+
+  //
+
+  window.addEventListener('resize', onWindowResize, false);
+
+}
+
+function onWindowResize() {
+
+  windowHalfX = window.innerWidth / 1;
+  windowHalfY = window.innerHeight / 1;
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  composer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
+function change_uvs(geometry, unitx, unity, offsetx, offsety) {
+
+  var uvs = geometry.attributes.uv.array;
+
+  for (var i = 0; i < uvs.length; i += 2) {
+
+    uvs[i] = (uvs[i] + offsetx) * unitx;
+    uvs[i + 1] = (uvs[i + 1] + offsety) * unity;
+
+  }
+
+}
+
+
+function onDocumentMouseMove(event) {
+
+  mouseX = (event.clientX - windowHalfX);
+  mouseY = (event.clientY - windowHalfY) * 0.03;
+
+}
+
+//
+
+function animate() {
+
+  requestAnimationFrame(animate);
+
+  render();
+
+}
+
+var h, counter = 1;
+
+function render() {
+
+  var time = Date.now() * 0.00005;
+
+  camera.position.x += (mouseX - camera.position.x) * 0.0003;
+  camera.position.y += (- mouseY - camera.position.y) * 0.0;
+
+  camera.lookAt(scene.position);
+
+  for (var i = 0; i < cube_count; i++) {
+
+    material = materials[i];
+
+    h = (360 * (material.hue + time) % 360) / 360;
+    material.color.setHSL(h, material.saturation, 1);
+
+  }
+
+  if (counter % 1000 > 200) {
+
+    for (var i = 0; i < cube_count; i++) {
+
+      mesh = meshes[i];
+
+      mesh.rotation.x += 50 * mesh.dx;
+      mesh.rotation.y += 50 * mesh.dy;
+
+      mesh.position.x -= 50 * mesh.dx;
+      mesh.position.y += 50 * mesh.dy;
+      mesh.position.z += 125 * mesh.dx;
+
+    }
+
+  }
+
+  if (counter % 2000 === 0) {
+
+    for (var i = 0; i < cube_count; i++) {
+
+      mesh = meshes[i];
+
+      mesh.dx *= - 1;
+      mesh.dy *= - 1;
+
+    }
+
+  }
+
+  counter++;
+
+  renderer.clear();
+  composer.render();
+
+}
+
